1230dce514029d0be62b0a47a111be3e7f8bdb72
Diego Rabatone Oliveira <diraol@ncc.unesp.br>
2017-02-13 16:58:29 -0200

    Fix kytos_auth decorator while handling authentication

--- kytos/utils/decorators.py
+++ kytos/utils/decorators.py
@@ -6,6 +6,8 @@ from urllib.parse import urljoin
 
 import requests
 
+from kytos.utils.config import KytosConfig
+
 log = logging.getLogger(__name__)
 
 
@@ -14,27 +16,43 @@ class kytos_auth:
 
     def __init__(self, func):
         self.func = func
-        self.config = func._config.config
+        self.config = KytosConfig().config
 
-    def __call__(self):
+    def __call__(self, *args, **kwargs):
         if not self.config.has_option('napps', 'uri'):
             self.config.set('napps', 'uri',
                             input("Enter the kytos napps server address: "))
 
         if not self.config.has_option('auth', 'user'):
-            self.config.set('auth', 'user', input("Enter the username: "))
+            user = input("Enter the username: ")
+            self.config.set('auth', 'user', user)
+        else:
+            user = self.config.get('auth', 'user')
 
         if not self.config.has_option('auth', 'token'):
-            self.authenticate()
+            token = self.authenticate()
+        else:
+            token = self.config.get('auth', 'token')
+
+        self.obj._config.set('auth', 'user', user)
+        self.obj._config.set('auth', 'token', token)
+        self.func.__call__(self.obj, *args, **kwargs)
+
+    def __get__(self, instance, owner):
+        self.cls = owner
+        self.obj = instance
+
+        return self.__call__
 
     def authenticate(self):
         endpoint = urljoin(self.config.get('napps', 'uri'), '/api/auth/')
-        username = self.config.get('auth', 'username')
-        password = getpass("Enter the password for %s: " % self.config.user)
+        username = self.config.get('auth', 'user')
+        password = getpass("Enter the password for {}: ".format(username))
         request = requests.post(endpoint, auth=(username, password))
         if request.status_code != 201:
-            log.error('%s: %s', request.status_code, request.reason)
-            sys.exit()
+            log.error('ERROR: %s: %s', request.status_code, request.reason)
+            sys.exit(1)
         else:
-            json = request.json()
-            self.config.save_token(username, json.get('hash'))
+            data = request.json()
+            KytosConfig().save_token(username, data.get('hash'))
+            return data.get('hash')
