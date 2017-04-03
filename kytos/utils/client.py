c12641c728c05698a19935328d7d4987fd6e55e9
Beraldo Leal <beraldo.ncc@gmail.com>
2017-04-03 10:34:12 -0300

    we are under MIT now

--- kytos/utils/client.py
+++ kytos/utils/client.py
@@ -5,20 +5,6 @@
 #
 # Authors:
 #    Beraldo Leal <beraldo AT ncc DOT unesp DOT br>
-#
-# kytos-utils is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# kytos-utils is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
-#
 
 import json
 import logging
