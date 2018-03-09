c4d1ce047ef9e18c40175c6df39eed47d2e58f2a
Macartur Sousa <macartur.sc@gmail.com>
2018-03-09 10:21:39 -0300

    Update version from 2018.1b1.dev0 to 2018.1b1

--- setup.py
+++ setup.py
@@ -144,7 +144,7 @@ class DevelopMode(develop):
 
 
 setup(name='kytos-utils',
-      version='2018.1b1.dev0',
+      version='2018.1b1',
       description='Command line utilities to use with Kytos.',
       url='http://github.com/kytos/kytos-utils',
       author='Kytos Team',
