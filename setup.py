e505cd5417105adb8f410202dd52947a251a820b
Humberto Diógenes <hdiogenes@gmail.com>
2018-12-14 13:53:18 -0300

    Version bump to 2018.2b3 + changelog

--- setup.py
+++ setup.py
@@ -162,7 +162,7 @@ class DevelopMode(develop, CommonInstall):
 
 
 setup(name='kytos-utils',
-      version='2018.2b2',
+      version='2018.2b3',
       description='Command line utilities to use with Kytos.',
       url='http://github.com/kytos/kytos-utils',
       author='Kytos Team',
