6c6c96f081a02c073bc69764364635f71fa58ff7
Beraldo Leal <beraldo.leal@cern.ch>
2018-12-21 11:20:31 -0200

    rc1 version bump

--- setup.py
+++ setup.py
@@ -162,7 +162,7 @@ class DevelopMode(develop, CommonInstall):
 
 
 setup(name='kytos-utils',
-      version='2018.2b3',
+        version='2018.2rc1',
       description='Command line utilities to use with Kytos.',
       url='http://github.com/kytos/kytos-utils',
       author='Kytos Team',
