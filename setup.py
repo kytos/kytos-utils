dd673087cf45314ddf37baebcd47aa249868f8da
Rogerio Motitsuki <rogerio.motitsuki@gmail.com>
2020-08-07 15:47:43 -0300

    Fix Coverage and Test not throwing errors

--- setup.py
+++ setup.py
@@ -104,6 +104,8 @@ class Test(TestCommand):
             check_call(cmd, shell=True)
         except CalledProcessError as exc:
             print(exc)
+            print('Unit tests failed. Fix the error(s) above and try again.')
+            sys.exit(-1)
 
 
 class TestCoverage(Test):
@@ -119,7 +121,9 @@ class TestCoverage(Test):
             check_call(cmd, shell=True)
         except CalledProcessError as exc:
             print(exc)
-
+            print('Coverate tests failed. Fix the errors above and try again.')
+            sys.exit(-1)
+            
 
 class CITest(TestCommand):
     """Run all CI tests."""
